c3e016cb85239457ac12813e735fa1d263a30ff0
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2025-09-06 22:15:06 -0400

    Added button to select grid size.
    Button prompts user to input grid size and validates answer

--- game.js
+++ game.js
@@ -1,10 +1,19 @@
 function createGrid() {
-  const size = 16;
+  let size = prompt("Enter a size up to 100 as a digit", 16);
+  size = Number(size);
+  console.log(size);
+  if (size > 100 || isNaN(size)) {
+    alert("Please enter a number less than 100.");
+    return;
+  }
+
   const container = document.querySelector(".container");
+  container.textContent = "";
 
   for (let i = 0; i < size; i++) {
     let column = document.createElement("div");
     column.classList.add("column");
+    column.style.backgroundColor = "white";
     for (let i = 0; i < size; i++) {
       let square = document.createElement("div");
       square.classList.add("square");
@@ -19,4 +28,5 @@ function createGrid() {
   );
 }
 
-createGrid();
+const button = document.querySelector("button");
+button.addEventListener("click", createGrid);
